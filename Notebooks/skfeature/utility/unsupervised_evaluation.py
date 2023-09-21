a8246bdee65fecc51b3c4b6b7215f17ba2942382
a-pino <alexander.pino@mats.ist.osaka-u.ac.jp>
2023-09-21 12:19:28 +0800

    fix linear_assignment_

--- Notebooks/skfeature/utility/unsupervised_evaluation.py
+++ Notebooks/skfeature/utility/unsupervised_evaluation.py
@@ -1,5 +1,5 @@
 import numpy as np
-import utils.linear_assignment_ as la
+import linear_assignment_ as la
 from sklearn.metrics import accuracy_score
 from sklearn.metrics.cluster import normalized_mutual_info_score
 from sklearn.cluster import KMeans
